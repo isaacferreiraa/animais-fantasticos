8cb137dae003b0f286e9ee7ad1a0b1a14e29788f
isaacferreiraa <aristocrata.black@gmail.com>
2024-08-01 17:28:21 -0300

    Navegação de conteúdo  por clicks na lista de animais

--- js/script.js
+++ js/script.js
@@ -1,16 +1,42 @@
-var linksInternos = document.querySelectorAll('a[href^="#"]')
+const tabMenu = document.querySelectorAll('.js-tabmenu li');
+const tabContent = document.querySelectorAll('.js-tabcontent section');
+
+if(tabMenu.length && tabContent.length) {
+    tabContent[0].classList.add('active')
 
 
-function scrollToSection(event) {
-    event.preventDefault();
-    const href = event.currentTarget.getAttribute('href');
-    const section = document.querySelector(href)
-    
-    section.scrollIntoView({
-        behavior: "smooth"
-    })
-}
+    function handleActiveTab(index) {
+        tabContent.forEach((section) => {
+            section.classList.remove('active')
+        })
+        tabContent[index].classList.add('active');
+    }
 
-linksInternos.forEach((link) => {   
-    link.addEventListener('click', scrollToSection);
-});
+    tabMenu.forEach((itemMenu, index) => {
+        itemMenu.addEventListener('click', () => {
+            handleActiveTab(index);
+        });
+    });
+};
+
+
+function scrollSuave() {
+    const linksInternos = document.querySelectorAll('a[href^="#"]');
+
+
+    function scrollToSection(event) {
+        event.preventDefault();
+        const href = event.currentTarget.getAttribute('href');
+        const section = document.querySelector(href);
+        
+        section.scrollIntoView({
+            behavior: "smooth"
+        })
+    }
+
+    linksInternos.forEach((link) => {   
+        link.addEventListener('click', scrollToSection);
+    });
+};
+
+scrollSuave();
