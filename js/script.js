8a7dfed6f3d2f70be38259dde41cea08e4ad20e7
isaacferreiraa <aristocrata.black@gmail.com>
2024-08-01 18:06:09 -0300

    favicon adicionado

--- js/script.js
+++ js/script.js
@@ -1,23 +1,32 @@
+
+
+
+
+
 const tabMenu = document.querySelectorAll('.js-tabmenu li');
 const tabContent = document.querySelectorAll('.js-tabcontent section');
 
-if(tabMenu.length && tabContent.length) {
-    tabContent[0].classList.add('active')
-
-
-    function handleActiveTab(index) {
-        tabContent.forEach((section) => {
-            section.classList.remove('active')
-        })
-        tabContent[index].classList.add('active');
-    }
-
-    tabMenu.forEach((itemMenu, index) => {
-        itemMenu.addEventListener('click', () => {
-            handleActiveTab(index);
+function tabContentMenu() {
+    if(tabMenu.length && tabContent.length) {
+        tabContent[0].classList.add('active')
+    
+    
+        function handleActiveTab(index) {
+            tabContent.forEach((section) => {
+                section.classList.remove('active')
+            })
+            tabContent[index].classList.add('active');
+        }
+    
+        tabMenu.forEach((itemMenu, index) => {
+            itemMenu.addEventListener('click', () => {
+                handleActiveTab(index);
+            });
         });
-    });
+    };
 };
+
+tabContentMenu()
 
 
 function scrollSuave() {
