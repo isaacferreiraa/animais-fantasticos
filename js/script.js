b6dc4cf1992c23861053191ffbae1f4e51c29b29
isaacferreiraa <aristocrata.black@gmail.com>
2024-08-01 07:01:07 -0300

    scroll suave adicinado com Javascript

---
+++ js/script.js
@@ -0,0 +1,16 @@
+var linksInternos = document.querySelectorAll('a[href^="#"]')
+
+
+function scrollToSection(event) {
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute('href');
+    const section = document.querySelector(href)
+    
+    section.scrollIntoView({
+        behavior: "smooth"
+    })
+}
+
+linksInternos.forEach((link) => {   
+    link.addEventListener('click', scrollToSection);
+});
